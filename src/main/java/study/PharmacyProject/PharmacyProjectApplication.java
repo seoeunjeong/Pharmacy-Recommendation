4825785467c81536806502cd57c88a99e4c84f40
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2023-09-03 09:29:04 +0900

    first commit

---
+++ src/main/java/study/PharmacyProject/PharmacyProjectApplication.java
@@ -0,0 +1,13 @@
+package study.PharmacyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/PharmacyProject/PharmacyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.PharmacyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
